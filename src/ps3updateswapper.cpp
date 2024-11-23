bde2af4543106dd3c5f3827f3e9489eb1af2fbf2
Tugra Kamer <tugrakamer@icloud.com>
2024-11-23 14:31:11 +0300

    Add error message, fix namespace inconsistency

--- src/ps3updateswapper.cpp
+++ src/ps3updateswapper.cpp
@@ -20,11 +20,18 @@ enum FirmwareType {
 int main() {
     bool fileExists;
     FirmwareType replace;
-    if ((!fs::exists(PS3_OFW_UPDATE_FILE) && !fs::exists(OFW_INDICATOR_FILE)) ||
-        (!fs::exists(PS3_HFW_UPDATE_FILE) && !fs::exists(HFW_INDICATOR_FILE)) ||
-        (!fs::exists(PS3_HFW_UPDATE_FILE) && !fs::exists(PS3_OFW_UPDATE_FILE)))
+
+    if (((!fs::exists(PS3_OFW_UPDATE_FILE) && !fs::exists(OFW_INDICATOR_FILE)) ||
+        (!fs::exists(PS3_HFW_UPDATE_FILE) && !fs::exists(HFW_INDICATOR_FILE)) &&
+        !(!fs::exists(PS3_ACTIVE_UPDATE_FILE) && !fs::exists(HFW_INDICATOR_FILE) && !fs::exists(OFW_INDICATOR_FILE))) ||
+        (!fs::exists(PS3_HFW_UPDATE_FILE) && !fs::exists(PS3_OFW_UPDATE_FILE))) {
+        std::cout << "Error: Missing file(s):" << std::endl;
+        std::cout << PS3_OFW_UPDATE_FILE << ": " << (fs::exists(PS3_OFW_UPDATE_FILE) ? "Found" : "Missing") << std::endl;
+        std::cout << PS3_HFW_UPDATE_FILE << ": " << (fs::exists(PS3_HFW_UPDATE_FILE) ? "Found" : "Missing") << std::endl;
+        std::cout << PS3_ACTIVE_UPDATE_FILE << ": " << (fs::exists(PS3_ACTIVE_UPDATE_FILE) ? "Found" : "Missing") << std::endl;
         return ENOENT;
-    if (std::filesystem::exists(OFW_INDICATOR_FILE)) {
+    }
+    if (fs::exists(OFW_INDICATOR_FILE)) {
         replace = HFW;
     } else {
         replace = OFW;
@@ -32,24 +39,24 @@ int main() {
     switch (replace)
     {
     case HFW:
-        std::filesystem::rename(OFW_INDICATOR_FILE, HFW_INDICATOR_FILE);
+        fs::rename(OFW_INDICATOR_FILE, HFW_INDICATOR_FILE);
         if (fs::exists(PS3_ACTIVE_UPDATE_FILE) && !fs::exists(PS3_OFW_UPDATE_FILE))
-            std::filesystem::rename(PS3_ACTIVE_UPDATE_FILE, PS3_OFW_UPDATE_FILE);
+            fs::rename(PS3_ACTIVE_UPDATE_FILE, PS3_OFW_UPDATE_FILE);
         else if (fs::exists(PS3_ACTIVE_UPDATE_FILE) && fs::exists(PS3_OFW_UPDATE_FILE))
-            std::filesystem::remove(PS3_ACTIVE_UPDATE_FILE);
-        std::filesystem::rename(PS3_HFW_UPDATE_FILE, PS3_ACTIVE_UPDATE_FILE);
+            fs::remove(PS3_ACTIVE_UPDATE_FILE);
+        fs::rename(PS3_HFW_UPDATE_FILE, PS3_ACTIVE_UPDATE_FILE);
         break;
     default:
-        if (!std::filesystem::exists(HFW_INDICATOR_FILE)) {
+        if (!fs::exists(HFW_INDICATOR_FILE)) {
                 std::ofstream(OFW_INDICATOR_FILE) << "\"This one's empty.\"";
         } else {
-            std::filesystem::rename(HFW_INDICATOR_FILE,OFW_INDICATOR_FILE);
+            fs::rename(HFW_INDICATOR_FILE,OFW_INDICATOR_FILE);
         }
         if (fs::exists(PS3_ACTIVE_UPDATE_FILE) && !fs::exists(PS3_HFW_UPDATE_FILE))
-            std::filesystem::rename(PS3_ACTIVE_UPDATE_FILE, PS3_HFW_UPDATE_FILE);
+            fs::rename(PS3_ACTIVE_UPDATE_FILE, PS3_HFW_UPDATE_FILE);
         else if (fs::exists(PS3_ACTIVE_UPDATE_FILE) && fs::exists(PS3_HFW_UPDATE_FILE))
-            std::filesystem::remove(PS3_ACTIVE_UPDATE_FILE);
-        std::filesystem::rename(PS3_OFW_UPDATE_FILE, PS3_ACTIVE_UPDATE_FILE);
+            fs::remove(PS3_ACTIVE_UPDATE_FILE);
+        fs::rename(PS3_OFW_UPDATE_FILE, PS3_ACTIVE_UPDATE_FILE);
         break;
     }
     return 0;
